b60edb8d24b180fbbbe4951a110696406bf5f05a
outsider1703 <simka-simka020697simka-simka1>
2020-07-01 14:33:19 +0200

    Initial Commit

---
+++ CoreDataStudyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataStudyProject
+//
+//  Created by Macbook on 01.07.2020.
+//  Copyright © 2020 Igor Simonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
